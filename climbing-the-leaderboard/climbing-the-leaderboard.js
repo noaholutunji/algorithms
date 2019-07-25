739cdad243735ea245b0a24e2cfb63025d8a03f1
Noah <osuolale49@gmail.com>
2019-07-25 10:30:39 +0100

    Climbing the Leaderboard Challenge Solved

---
+++ climbing-the-leaderboard/climbing-the-leaderboard.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the climbingLeaderboard function below.
+function climbingLeaderboard(scores, alice) {
+    let positions = [];
+    let distScore = [...new Set(scores)];
+    let aliceIndex = 0;
+    let i = distScore.length - 1;
+    alice.forEach(element => {
+
+        while (i >= 0) {
+            if (element >= distScore[i]) {
+                i--;
+            } else {
+                positions.push(i + 2);
+                break;
+            }
+        }
+        if (i < 0) positions.push(1);
+
+    });
+
+    return positions;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const scoresCount = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const aliceCount = parseInt(readLine(), 10);
+
+    const alice = readLine().split(' ').map(aliceTemp => parseInt(aliceTemp, 10));
+
+    let result = climbingLeaderboard(scores, alice);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
